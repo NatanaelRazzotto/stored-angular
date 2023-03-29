732153fa67032ad8371dec7f5d0564a2233142fa
NATANAEL RAZZOTTO <natanael8447@outlook.com>
2023-03-29 15:28:40 -0300

    page not found interface

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NaoencontradoComponent } from './naoencontrado/naoencontrado.component';
 
 const routes : Routes = [{ path: 'produtos', loadChildren: () => import('./produtos/produtos.module').then(m => m.ProdutosModule) },
-{   path:"", redirectTo: "produtos", pathMatch: "full"}];
+{   path:"", redirectTo: "produtos", pathMatch: "full"},
+{   path:"**", component:NaoencontradoComponent}];
 
 @NgModule({
   declarations: [],
